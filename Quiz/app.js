b58dd901c1aa71f9f3437a765ddfc142c8b61ba1
samy-15 <84139411+samy-15@users.noreply.github.com>
2022-02-07 00:00:15 +0530

    Add files via upload

---
+++ Quiz/app.js
@@ -0,0 +1,96 @@
+let rightAns,
+    rightNumber = 0,
+    falseNumber = 0;
+
+document.addEventListener('DOMContentLoaded', function () {
+    addQuestion();
+
+    eventListeners();
+});
+
+eventListeners = () => {
+    document.querySelector('#check').addEventListener('click', validateAnswer);
+
+}
+addQuestion = () => {
+    const url = 'https://opentdb.com/api.php?amount=10&category=17&difficulty=easy';
+    fetch(url)
+        .then(data => data.json())
+        .then(result => showQuestion(result.results));
+}
+showQuestion = questions => {
+
+    const questionHTML = document.createElement('div');
+    questionHTML.classList.add('col-12');
+
+    questions.forEach(question => {
+        rightAns = question.correct_answer;
+
+        let possibleAnswers = question.incorrect_answers;
+        possibleAnswers.splice(Math.floor(Math.random() * 3), 0, rightAns);
+
+        questionHTML.innerHTML = `<div class="row justify-content-between heading">
+        <p class="category">Category:${question.category}</p>
+        <div class="btn-group btn-group-lg" role="group" aria-label="...">
+        <button type="button" class="btn btn-danger">${falseNumber}</button>
+        <button type="button" class="btn btn-success">${rightNumber}</button>
+        </div>
+        <div>
+        <h2 class="text-center">${question.question}`
+
+        const answerDiv = document.createElement('div');
+        answerDiv.classList.add('questions', 'row', 'justify-content-around', 'mt-5');
+        possibleAnswers.forEach(answer => {
+            const answerHTML = document.createElement('li');
+            answerHTML.classList.add('col-12', 'col-md-5');
+            answerHTML.textContent = answer;
+
+            answerHTML.onclick = selectAnswer;
+
+            answerDiv.appendChild(answerHTML);
+        })
+        questionHTML.appendChild(answerDiv); /*Main wrapper */
+
+        document.querySelector('#app').appendChild(questionHTML);
+    })
+}
+selectAnswer = (e) => {
+    if (document.querySelector('.active')) {
+        const activeAnswer = document.querySelector('.active');
+        activeAnswer.classList.remove('active');
+    }
+
+    e.target.classList.add('active');
+}
+validateAnswer = () => {
+    if (document.querySelector('.questions .active')) {
+
+        verifyAnswer();
+
+    } else {
+        const errorDiv = document.createElement('div');
+        errorDiv.classList.add('alert', 'alert-danger', 'col-md-6');
+        errorDiv.textContent = "Please select Answer";
+        const questionsDiv = document.querySelector('.questions');
+        questionsDiv.appendChild(errorDiv);
+
+        setTimeout(() => {
+            document.querySelector('.alert-danger').remove();
+        }, 2000);
+
+    }
+}
+verifyAnswer = () => {
+    const userAnswer = document.querySelector('.questions .active');
+    if (userAnswer.textContent === rightAns) {
+        rightNumber++;
+    } else {
+        falseNumber++;
+    }
+
+    const app = document.querySelector('#app');
+    while (app.firstChild) {
+        app.removeChild(app.firstChild);
+    }
+    addQuestion();
+}
